d699871d7884629cff8e94f6f5c452436393f99b
165 <165@DESKTOP-7Q69ASV>
2023-02-20 18:01:16 +0900

    스프링 시큐리티 테스트

---
+++ src/main/java/org/zerock/security/CustomNoOpPasswordEncoder.java
@@ -0,0 +1,23 @@
+package org.zerock.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomNoOpPasswordEncoder implements PasswordEncoder{
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		// TODO Auto-generated method stub
+		log.info("before encode"+rawPassword);
+		return rawPassword.toString();
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
